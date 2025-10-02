135ba271f2149596b23925c009319b1ef845279e
Nathan-Byrne <c00281281@setu.ie>
2025-10-02 12:46:24 +0100

    Update Button.svelte

--- merge-conflicts/svelte-merge-conflits/src/lib/Button.svelte
+++ merge-conflicts/svelte-merge-conflits/src/lib/Button.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
     export let label: string = "Submit"; // Changed Default
+    export let secondText : string = "FromBranchB";
     export let variant: "primary" | "secondary" = "primary"; // New Prop
     export let disabled: boolean = false;
     const dispatch = createEventDispatcher<{ pressed: { at: number }}>();
@@ -12,11 +13,12 @@
 
 <button class="btn" data-variant={variant} {disabled} on:click={() => console.log("Clicked")}>
     {label}
+    {secondText}
 </button>
 
 <style>
     .btn {
-        padding: 0.5rem 1rem;
+        padding: 0.75rem 1rem;
         color: white;
         border-radius: 0.375rem;
         opacity: var(--opacity, 1);
@@ -25,5 +27,5 @@
     .btn:disabled { --opacity: 0.6; cursor: not-allowed; }
 
     .btn[data-variant="primary"] { background: #2563EB; } /* Blue-600 */
-    .btn[data-variant="secondary"] { background: #6B7280; } /* Gray-500 */
+    .btn[data-variant="secondary"] { background: #00ff2a; } /* Lime Green */
 </style>
