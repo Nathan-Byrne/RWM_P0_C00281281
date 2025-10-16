c0b061e27ec118321c85fce944e2086d3a036096
Jake Fitzgerald <115044918+Jake-Fitzgerald@users.noreply.github.com>
2025-10-16 21:22:05 +0100

    Changed CSS for storybook page for Part 2 Task rebaseing

--- Sprint 7/src/lib/components/ChecklistItems.svelte
+++ Sprint 7/src/lib/components/ChecklistItems.svelte
@@ -2,13 +2,15 @@
   import './ChecklistItems.css';
 
   // Button Data
-  type ButtonData = {
+  type ButtonData = 
+  {
     label: string;
     active: boolean;
   };
 
   // Buttons to Update the Bar
-  let buttons: ButtonData[] = [
+  let buttons: ButtonData[] = 
+  [
     { label: '1st Task', active: false },
     { label: '2nd Task', active: false },
     { label: '3rd Task', active: false },
@@ -16,11 +18,17 @@
     { label: '5th Task', active: false }
   ];
 
+  
+
   // Bar Percent
   let progress = 0;
+  // Whenever progress bar changes we update this with a dollar sign
+  $: isHalfComplete = progress >= 50;
+  let enableTooltip = false;
 
   // Log Percent Updates on Press
-  function toggleButton(index: number) {
+  function toggleButton(index: number) 
+  {
     buttons[index].active = !buttons[index].active;
     buttons = [...buttons];
   }
@@ -32,11 +40,60 @@
   }
 </script>
 
+<style>
+/* Tooltip container */
+.tooltip 
+{
+  position: relative;
+  display: inline-block;
+  border-bottom: 1px dotted black; /* Add dots under the hoverable text */
+  cursor: pointer;
+}
+
+/* Tooltip text */
+.tooltiptext 
+{
+  visibility: hidden; /* Hidden by default */
+  width: 130px;
+  background-color: black;
+  color: blue;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 6px;
+  position: absolute;
+  z-index: 1; /* Ensure tooltip is displayed above content */
+}
+
+/* Show the tooltip text on hover */
+.tooltip:hover .tooltiptext 
+{
+    visibility: visible;
+}
+</style>
+
+
+<div class:half-complete = {isHalfComplete}>
+  Hover over me
+  <span class="tooltiptext">You're 50% of the way there! (milestone)</span>
+</div>
+
+
+
+
 <div class="container">
   <!-- Bar -->
-  <div class="progress-bar">
+  <div class="progress-bar" aria-hidden="true"
+  on:mousemove={() => enableTooltip = true}
+  on:mouseout={() => enableTooltip = false}
+  on:blur={() => enableTooltip = false}>
     <div class="progress" style="width: {progress}%;"></div>
   </div>
+
+  {#if enableTooltip && progress >= 50}
+    <div>
+      <span>Tooltip: 50% or more</span>
+    </div>
+  {/if}
 
   <!-- Task Buttons -->
   <div class="buttons">
@@ -44,6 +101,13 @@
 
       <button class:active={btn.active} on:click={() => toggleButton(i)}>
         {btn.label}
+
+        {#if btn.active}
+        <!-- &#10003 is a tickmark character -->
+        <!-- aria-hidden="true" hides accessiblity modes for screen readers (text to speech?) from appearing -->
+          <span class = "tick"  aria-hidden="true">&#10003</span>
+        {/if}
+        
       </button>
 
     {/each}
@@ -51,4 +115,6 @@
 
   <!-- Submit Button-->
   <button class="submit" on:click={submitProgress}>Submit</button>
+  
+  <p>Task &#10003;</p>
 </div>
